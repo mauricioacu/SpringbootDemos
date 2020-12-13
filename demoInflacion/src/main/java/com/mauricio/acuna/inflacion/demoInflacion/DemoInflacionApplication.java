ee21546388ffdb3909db8d4af71b25aa4217063e
mauricioacu <mauricioacu@users.noreply.github.com>
2020-12-13 02:20:54 -0300

    Add files via upload
    Subida con primeras configuraciones

---
+++ demoInflacion/src/main/java/com/mauricio/acuna/inflacion/demoInflacion/DemoInflacionApplication.java
@@ -0,0 +1,13 @@
+package com.mauricio.acuna.inflacion.demoInflacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInflacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoInflacionApplication.class, args);
+	}
+
+}
---
+++ demoInflacion/src/test/java/com/mauricio/acuna/inflacion/demoInflacion/DemoInflacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauricio.acuna.inflacion.demoInflacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInflacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
